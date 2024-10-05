65fe24a8c55d772f4e3e3057ff258d8d1394acd2
ReCore-sys <kjshore2004@gmail.com>
2024-10-06 05:37:15 +1030

    Can run JVM methods. Yay!

--- src/lib/plugins/src/lib.rs
+++ src/lib/plugins/src/lib.rs
@@ -1,19 +1,20 @@
 pub mod errors;
-use jni::{JavaVM, InitArgsBuilder, JNIVersion};
+use crate::errors::PluginsError;
+use ferrumc_utils::root;
 use jni::objects::JObject;
+use jni::{AttachGuard, InitArgsBuilder, JNIEnv, JNIVersion, JavaVM};
+use std::sync::Arc;
 use tracing::error;
 use which::which;
-use ferrumc_utils::root;
-use crate::errors::PluginsError;
 
-pub fn load_plugins() -> Result<(), PluginsError> {
+pub fn load_plugins() -> Result<Arc<JavaVM>, PluginsError> {
     if which("java").is_err() {
         error!("Java not found in PATH");
         return Err(PluginsError::JVMError("Java not found in PATH".to_string()));
     }
     // IF IT BREAKS
     // 1. Make sure you have java installed
-    // 2. Make sure a full java install is in your PATH (You need more than just the 3 exes in 
+    // 2. Make sure a full java install is in your PATH (You need more than just the 3 exes in
     // C:\Program Files (x86)\Common Files\Oracle\Java\javapath, check C:\Program Files\Java\jdk-23\bin
     // and maybe add it to your PATH if needed)
     // 3. Try setting JAVA_HOME to the path of your java install
@@ -21,33 +22,38 @@ pub fn load_plugins() -> Result<(), PluginsError> {
         .version(JNIVersion::V8)
         .option("-Xcheck:jni")
         .build()
-        .map_err(
-            |e| PluginsError::JVMError(format!("Failed to create JVM args: {}", e))
-        )?;
-    let jvm = JavaVM::new(jvm_args).map_err(
-        |e| PluginsError::JVMError(format!("Failed to create JVM: {}", e))
-    )?;
-    let mut env = jvm.attach_current_thread().
-        map_err(
-            |e| PluginsError::JVMError(format!("Failed to attach to JVM: {}", e))
-        )?;
+        .map_err(|e| PluginsError::JVMError(format!("Failed to create JVM args: {}", e)))?;
+    let jvm = Arc::new(
+        JavaVM::new(jvm_args)
+            .map_err(|e| PluginsError::JVMError(format!("Failed to create JVM: {}", e)))?,
+    );
+    jvm.attach_current_thread_permanently()
+        .map_err(|e| PluginsError::JVMError(format!("Failed to attach to JVM: {}", e)))?;
     let class_files = std::fs::read_dir(root!(".etc/plugins")).unwrap();
+    let mut env = jvm.get_env().unwrap();
     for dir_file in class_files.flatten() {
         let file_name = dir_file.file_name();
         let file_name = file_name.to_str().unwrap();
         if file_name.ends_with(".class") {
             let data = std::fs::read(dir_file.path()).unwrap();
-            env.define_class(file_name, &JObject::null(), &data).unwrap();
+            env.define_unnamed_class(&JObject::null(), &data)
+                .map_err(|e| PluginsError::JVMError(format!("Failed to load class: {}", e)))?;
             println!("Loaded: {}", file_name);
         }
     }
-    Ok(())
+    Ok(jvm.clone())
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_setup() {
-        super::load_plugins().unwrap();
+        println!("Mem use before: {}", memory_stats::memory_stats().unwrap().physical_mem);
+        let jvm = super::load_plugins().unwrap();
+        println!("Mem use during: {}", memory_stats::memory_stats().unwrap().physical_mem);
+        let mut env = jvm.get_env().unwrap();
+        env.call_static_method("com/ferrumc/MainKt", "setup", "()V", &[])
+            .unwrap();
+        println!("Mem use after: {}", memory_stats::memory_stats().unwrap().physical_mem);
     }
 }
